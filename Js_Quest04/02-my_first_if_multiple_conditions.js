4a0ccc598dcea9107c4c8bed034986f1d81cb16f
Bakare Muideen Adeleke <bakaremuideenadeleke@yahoo.com>
2023-01-20 23:16:32 +0100

    Task completed

---
+++ Js_Quest04/02-my_first_if_multiple_conditions.js
@@ -0,0 +1,13 @@
+var a = 10;
+var b = 9;
+var c = 11;
+var d = 10;
+var y = 9;
+var z = 11;
+
+if (a) {
+  console.log("a is bigger than b AND smaller than c AND equal to d");
+}
+if (z) {
+  console.log("z OR y are bigger than a");
+}
