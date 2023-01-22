be80220998fe1a230e734157a133bbc686c4bbe0
Bakare Muideen Adeleke <bakaremuideenadeleke@yahoo.com>
2023-01-22 22:36:29 +0100

    Task completed

---
+++ Js_Quest05/01-my_is_negative.js
@@ -0,0 +1,12 @@
+function my_is_negative(n) {
+  if (n < 0) {
+    return 0;
+  }
+  else {
+    return 1;
+  }
+}
+
+// console.log(my_is_negative(-1));
+// console.log(my_is_negative(1));
+// console.log(my_is_negative(0));
