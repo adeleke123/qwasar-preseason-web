75c2d437eb994ab44711eb77f3cfbea3a9f49465
Bakare Muideen Adeleke <bakaremuideenadeleke@yahoo.com>
2023-01-22 22:47:55 +0100

    Task completed

---
+++ Js_Quest05/06-my_string_formatting.js
@@ -0,0 +1,7 @@
+const firstname = "john";
+const lastname = "doe";
+let age = 37;
+
+function my_string_formatting(firstname, lastname, age) {
+    console.log(`Hello, my name is ${firstname} ${lastname}, I'm ${age}.`);
+}
