dc8403a426791eea2d767cbbb8057285ef9ee4ee
Bakare Muideen Adeleke <bakaremuideenadeleke@yahoo.com>
2023-01-24 22:37:04 +0100

    Quest completed

---
+++ Js_Quest07/00-my_map_mult_two.js
@@ -0,0 +1,12 @@
+function my_map_mult_two(arr) {
+
+    let originalArr = [];
+
+    for (var i = 0; i < arr.length; i++) 
+    {
+       originalArr.push(arr[i]*2);
+
+    }
+    return originalArr;
+
+};
