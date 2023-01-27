00c49bd0f596883f1a2bbad40c573a7ac193a626
Bakare Muideen Adeleke <51156057+adeleke123@users.noreply.github.com>
2023-01-27 22:33:07 +0100

    Update app.js

--- My-First-Backend/app.js
+++ My-First-Backend/app.js
@@ -56,3 +56,24 @@ app.use("/protected", (req, res, next) => {
 app.listen(8080, () =>{
     console.log("server started on 8080")
 },'0.0.0.0')
+
+// Program Explanation
+This code creates a server using the Express.js framework, which is a popular web application framework for Node.js.
+
+The first line imports the Express.js module, which is required to create the server.
+
+The next line creates an instance of the Express.js application, which is stored in the "app" variable.
+
+The following line imports the express-basic-auth package which is used for basic authentication
+
+Next, there are several "app.get()" routes defined, each with a different path and corresponding callback function.
+
+When a client makes a GET request to the "/" route, a random song is selected from an array of songs and sent as the response.
+"/birth_date" route sends December 12, 1915 as response
+"/birth_city" sends Hoboken, New Jersey as response
+"/wives" sends Nancy Barbato, Ava Gardner, Mia Farrow, Barbara Marx as response
+"/picture" redirects to an image of Frank Sinatra
+"/public" sends "Everybody can see this page" as response
+Next, there is a route "/protected" that is commented out but the logic for basic authentication is in place. Here, it checks for the login and password credentials to be present in the header and matches it with the predefined credentials. If successful, sends "Welcome, authenticated client" as response and allows the client to move to next route. If not sends "401 Not authorized" as response
+
+Lastly, the server is set to listen on port 8080 and starts the server.
