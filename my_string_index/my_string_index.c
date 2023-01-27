85d56571b971911bc4f092553e063b2ce5f65aba
Bakare Muideen Adeleke <51156057+adeleke123@users.noreply.github.com>
2023-01-27 23:29:57 +0100

    Update my_string_index.c

--- my_string_index/my_string_index.c
+++ my_string_index/my_string_index.c
@@ -9,3 +9,10 @@ return i;
 }
 return -1;
 }
+/*
+This function will take in the haystack (a string) and the needle (a character) as parameters. 
+It will use a for loop to iterate through each character in the haystack, 
+and within the loop it will check if the current character is equal to the needle. 
+If it is, the function will return the current index (i) of the loop. 
+If the loop completes and the needle is not found in the haystack, the function will return -1.
+*/
